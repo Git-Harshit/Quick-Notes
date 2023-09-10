1fb9af04e535f9da3c6ece8e47cb349c7564515b
Harshit Gupta <hkg1412@gmail.com>
2023-09-10 19:17:25 +0530

    Initiate Quick_Notes application content
    Update existing React boilerplate generated using create-react-app to
    reflect Quick Notes components and file content. Correct app name in
    index.html page title, public/manifest.json name. Set source files
    indentation using tabs and initiate beginning content for Quick Notes
    application with two components in App.js. Check "Quick Notes" presence
    in screen with App.test.js for a basic test.
    Also set ServiceWorker using .register() in index.js file.
    
    Signed-off-by: Harshit Gupta <hkg1412@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+// Dependencies
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Components
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	var local_storage_key = "Quick_Notes";
+	var [notes, setNotes] = useState({});	// State Hook for notes in JSON
+
+	// Effect Hook to load notes from sessionStorage
+	useEffect(() => {
+		let local_notes = localStorage.getItem(local_storage_key);
+		if (local_notes !== null && local_notes !== undefined) {
+			try {
+				local_notes = JSON.parse(local_notes);
+			}
+			catch (error_information) {
+				console.error(error_information);
+				local_notes = {};
+			}
+			setNotes( { ...notes, ...local_notes } ); 
+		}
+		else {
+			localStorage.setItem(local_storage_key, JSON.stringify({}));	// .removeItem() before this .setItem() should be optional
+		}
+	}, []);
+
+	return (
+		<div className="Application">
+			<header className="App-header"></header>
+			<section className='contents'>
+				<h1>Quick Notes</h1>
+				{Object.keys(notes).map( (title, index) => 
+					<NotesCard title={title} detail={notes[title]} key={index}/>
+				)}
+			</section>
+			<footer className="App-footer">
+				<img src={logo} className="react-logo" alt="React"/>
+				<p> Created using &#9;{/* &tab; = &#0009; */}
+					<a className="link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a>
+				</p>
+			</footer>
+		</div>
+	);
+}
+
+class NotesCard extends React.Component {
+	render() {
+		return (
+			<article className="note-card">
+				<h4 className="note-title">{this.props.title}</h4>
+				<p className="note-detail">{this.props.detail}</p>
+			</article>
+		)
+	}
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders Title', () => {
+	render(<App />);
+	const linkElement = screen.getByText(/Quick Notes/i);
+	expect(linkElement).toBeInTheDocument();
 });
