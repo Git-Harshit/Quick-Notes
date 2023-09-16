64215ad26bbff4177e25ec73d85936c806d66503
Harshit Gupta <hkg1412@gmail.com>
2023-09-16 21:09:15 +0530

    Add New Note and Delete All buttons
    Adds two new buttons to add a note and delete all notes stored in order
    to allow addition and removal of notes from data storage. This makes the
    website application functionally working.
    Also improves page CSS for minimal view improvements, with
    responsiveness and more such improvements planned for later.
    
    Signed-off-by: Harshit Gupta <hkg1412@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Global Variable(s) for this file
+var local_storage_key = "Quick_Notes";
+
 // Components
 	// App component for Single Page Application (SPA) home
 function App() {
-	var local_storage_key = "Quick_Notes";
 	var [notes, setNotes] = useState([]);	// State Hook for notes in JSON
 
 	// Effect Hook to load notes from sessionStorage
 	useEffect(() => {
-		let local_notes = localStorage.getItem(local_storage_key);
-		if (local_notes !== null && local_notes !== undefined) {
-			try {
-				local_notes = JSON.parse(local_notes);
+		let local_notes = [];
+		try {
+			local_notes = localStorage.getItem(local_storage_key);
+			if ( !([null, undefined].includes(local_notes)) ) local_notes = JSON.parse(local_notes);
+			else {
+				// Initializing with empty array as 'null' from getItem indicates that no such entry with the given key exists
+				localStorage.setItem(local_storage_key, JSON.stringify([]));	// .removeItem() to clear before this .setItem() should be optional
 			}
-			catch (error_information) {
-				console.error(error_information);
-				local_notes = [];
-			}
-			setNotes( notes.concat(local_notes) );
 		}
-		else {
-			localStorage.setItem(local_storage_key, JSON.stringify([]));	// .removeItem() before this .setItem() should be optional
+		catch (error_information) {
+			console.error(error_information);
+			local_notes = [];
 		}
+		setNotes( (previous_notes) => previous_notes.concat(local_notes) );	// Update notes based on previous state
 	}, []);
 
 	return (
@@ -37,6 +39,19 @@ function App() {
 				{notes.map( (note, index) => 
 					<NotesCard title={note.title} detail={note["content"]} className={note["custom_properties"] ? note["custom_properties"]:"default"} key={index} />
 				)}
+				<article className="options">
+					<button className="option" id="addition" onClick={ () => { 
+						let title=prompt("Enter a brief title:"); 
+						let content=prompt("Enter single-line content:"); 
+						if (title || content) { 
+							setNotes(notes.concat([{title, content, custom_properties:""}])); 
+							localStorage.setItem(local_storage_key, JSON.stringify(notes)) 
+						}
+					} }>
+						Add Note
+					</button>
+					<button className="option" id="clearance" onClick={ () => { let decision = window.confirm("Are you sure?\nPlease note that this action is not reversible and will delete all your local notes stored here."); if (decision === true) localStorage.clear(local_storage_key) } }>Delete All</button>
+				</article>
 			</section>
 			<footer className="App-footer">
 				<img src={logo} className="react-logo" alt="React"/>
