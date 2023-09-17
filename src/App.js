69036edea9604f2a54f3e0c7421c9d2e4b350883
Harshit Gupta <hkg1412@gmail.com>
2023-09-17 22:35:43 +0530

    Update localStorage setItem after setNotes render
    Uses new Effect with dependency on 'notes' to save to localStorage on
    completion of asynchronous setNotes() updation, which fixes issues with
    some unupdated or missing last note from data when localStorage was
    updated immediately after calling setNotes before notes collection got
    updated. Current updation works well in production release build, but
    double useEffect calls in development mode for App function seem to be
    out of sync causing localStorage to get assigned with empty array of
    notes which clears the storage, this was fixed by assigning notes with
    local_notes which raises a warning.
    Boolean true checks before usage as in "notes && ..." fixes test run.
    Also involves some related source changes and some CSS improvements.
    
    Signed-off-by: Harshit Gupta <hkg1412@gmail.com>

--- src/App.js
+++ src/App.js
@@ -11,23 +11,30 @@ var local_storage_key = "Quick_Notes";
 function App() {
 	var [notes, setNotes] = useState([]);	// State Hook for notes in JSON
 
-	// Effect Hook to load notes from sessionStorage
+	// Effect Hook to load notes from sessionStorage (runs just once on initial render with empty dependency array as second argument to useEffect)
 	useEffect(() => {
 		let local_notes = [];
 		try {
 			local_notes = localStorage.getItem(local_storage_key);
 			if ( !([null, undefined].includes(local_notes)) ) local_notes = JSON.parse(local_notes);
-			else {
-				// Initializing with empty array as 'null' from getItem indicates that no such entry with the given key exists
-				localStorage.setItem(local_storage_key, JSON.stringify([]));	// .removeItem() to clear before this .setItem() should be optional
-			}
 		}
 		catch (error_information) {
 			console.error(error_information);
 			local_notes = [];
 		}
-		setNotes( (previous_notes) => previous_notes.concat(local_notes) );	// Update notes based on previous state
+		notes = local_notes;	// This is a temporary work-around for development run to prevent clearance of notes as observed during double executions of the following Effect. Production build already works fine directly without this warning-generating assignment required.
+		setNotes( local_notes );
 	}, []);
+
+	// Effect Hook to save notes to sessionStorage on every update and render by setNotes()
+	useEffect(function() {
+		try {
+			localStorage.setItem(local_storage_key, JSON.stringify(notes));	// .removeItem() to clear before this .setItem() should be optional
+		}
+		catch (exception_information) {
+			console.error(exception_information);
+		}
+	}, [notes]);
 
 	return (
 		<div className="Application">
@@ -36,21 +43,26 @@ function App() {
 			</header>
 			<section className="contents">
 				<h1>Quick Notes</h1>
-				{notes.map( (note, index) => 
+				{notes && notes.map( (note, index) => 
+					note && typeof(note) === typeof(Object()) && 
 					<NotesCard title={note.title} detail={note["content"]} className={note["custom_properties"] ? note["custom_properties"]:"default"} key={index} />
 				)}
 				<article className="options">
 					<button className="option" id="addition" onClick={ () => { 
 						let title=prompt("Enter a brief title:"); 
 						let content=prompt("Enter single-line content:"); 
-						if (title || content) { 
-							setNotes(notes.concat([{title, content, custom_properties:""}])); 
-							localStorage.setItem(local_storage_key, JSON.stringify(notes)) 
+						if (title || content) {
+							setNotes(previous_notes => previous_notes.concat([{title, content, custom_properties:""}]));	// setting 'notes' by updating from previous state
 						}
 					} }>
 						Add Note
 					</button>
-					<button className="option" id="clearance" onClick={ () => { let decision = window.confirm("Are you sure?\nPlease note that this action is not reversible and will delete all your local notes stored here."); if (decision === true) localStorage.clear(local_storage_key) } }>Delete All</button>
+					<button type="reset" className="option" id="clearance" onClick={ () => {
+						let decision = window.confirm("Are you sure?\nPlease note that this action is not reversible and will delete all your notes stored locally."); 
+						if (decision === true) { setNotes([]); } 
+					} }>
+						Delete All
+					</button>
 				</article>
 			</section>
 			<footer className="App-footer">
@@ -67,7 +79,7 @@ function App() {
 class NotesCard extends React.Component {
 	render() {
 		return (
-			<article className="note-card">
+			<article className={"note-card " + this.props.className}>
 				<h4 className="note-title">{this.props.title}</h4>
 				<p className="note-detail">{this.props.detail}</p>
 			</article>
